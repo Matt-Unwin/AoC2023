e1969dd3a979d4ed59847c3996c3c06fb2be1564
Matt Unwin <matthewunwin987@gmail.com>
2023-12-04 08:58:12 +0000

    d4 pt1

---
+++ days/d4/d4.py
@@ -0,0 +1,42 @@
+import re
+
+
+def d4_read():
+    dct_store = {}
+    for x in lines:
+        parts_2 = x.split(":")
+        game_no = parts_2[0].split()[1].strip()
+
+        winning_numbers = parts_2[1].split("|")[0].strip().split()
+        chosen_numbers = parts_2[1].split("|")[1].strip().split()
+
+        dct_store[game_no] = (winning_numbers, chosen_numbers)
+    return dct_store
+
+
+def pt1():
+    dct_store = d4_read()
+    sum_total = 0
+    for game, (winning_numbers, chosen_numbers) in dct_store.items():
+        card_points = 0
+        for chosen_number in chosen_numbers:
+            if chosen_number in winning_numbers:
+                if card_points == 0:
+                    card_points = 1
+                else:
+                    card_points *= 2
+        sum_total += card_points
+    return sum_total
+
+
+def pt2():
+    dct_store = d4_read()
+    sum_total = 0
+    return sum_total
+
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        lines = f.read().splitlines()
+    print("part1: " + str(pt1()))
+    print("part2: " + str(pt2()))
